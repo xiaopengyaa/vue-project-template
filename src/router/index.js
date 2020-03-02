d89b7071426aae0e04a398f14e25dbc3e59b7928
xiaopengyaa <857763476@qq.com>
2020-03-02 18:49:16 +0800

    feat: router、store、axios配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@views/About.vue')
-  }
-]
+// 动态路由引入
+const importRoutesAll = routesContext => {
+  let list = []
+  routesContext.keys().forEach(key => {
+    list = list.concat(routesContext(key).default)
+  })
+  return list
+}
+const routes = importRoutesAll(require.context('./', false, /\.routes\.js$/))
 
 const router = new VueRouter({
   routes
+})
+
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  store.dispatch('base/RESET_LOADING')
+  next()
 })
 
 export default router
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,23 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// 动态store引入
+const importStoreAll = storeContext => {
+  const reg = /\.\/([\s\S]+)\.store\.js$/
+  const storeObj = {}
+  storeContext.keys().forEach(key => {
+    const matchInfo = reg.exec(key)
+    if (matchInfo) {
+      const moduleName = matchInfo[1]
+      storeObj[moduleName] = storeContext(key).default
+    }
+  })
+  return storeObj
+}
+const modules = importStoreAll(
+  require.context('./modules', false, /\.store\.js$/)
+)
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules
 })
